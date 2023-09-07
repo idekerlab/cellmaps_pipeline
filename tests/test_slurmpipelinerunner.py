71f24a41fc84a488f5b84efc5f0ef84713b4292b
Joanna <jlenkiewicz@ucsd.edu>
2023-09-07 15:19:12 -0700

    Implemented unit tests for slurm generate functions

--- tests/test_slurmpipelinerunner.py
+++ tests/test_slurmpipelinerunner.py
@@ -38,3 +38,68 @@ class TestSLURMPipelineRunner(unittest.TestCase):
             self.assertEqual('#!/bin/bash', data[0])
         finally:
             shutil.rmtree(temp_dir)
+
+    def test_generate_download_images_command(self):
+        temp_dir = tempfile.mkdtemp()
+        cm4ai_image = 'test_image'
+        provenance = 'test_provenance'
+        try:
+            myobj = SLURMPipelineRunner(temp_dir, cm4ai_image=cm4ai_image, provenance=provenance)
+            filename = os.path.join(temp_dir, 'imagedownloadjob.sh')
+            myobj._generate_download_images_command()
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_download_ppi_command(self):
+        temp_dir = tempfile.mkdtemp()
+        samples = 'test_samples'
+        unique = 'test_unique'
+        provenance = 'test_provenance'
+        try:
+            myobj = SLURMPipelineRunner(temp_dir, samples=samples, unique=unique, provenance=provenance)
+            filename = os.path.join(temp_dir, 'ppidownloadjob.sh')
+            myobj._generate_download_ppi_command()
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_embed_image_command(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            myobj = SLURMPipelineRunner(temp_dir)
+            filename = os.path.join(temp_dir, 'imageembedjob1.sh')
+            myobj._generate_embed_image_command()
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_embed_ppi_command(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            myobj = SLURMPipelineRunner(temp_dir)
+            filename = os.path.join(temp_dir, 'ppiembedjob.sh')
+            myobj._generate_embed_ppi_command()
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_coembed_command(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            myobj = SLURMPipelineRunner(temp_dir)
+            filename = os.path.join(temp_dir, 'coembeddingjob1.sh')
+            myobj._generate_coembed_command(fold=1)
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_hierarchy_command(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            myobj = SLURMPipelineRunner(temp_dir)
+            filename = os.path.join(temp_dir, 'hierarchyjob.sh')
+            myobj._generate_hierarchy_command()
+            self.assertTrue(os.path.isfile(filename))
+        finally:
+            shutil.rmtree(temp_dir)
