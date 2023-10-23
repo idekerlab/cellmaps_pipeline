f12f5c00651fc4ce4644389af5664d746473c66d
Chris Churas <churas.camera@gmail.com>
2023-10-23 11:34:10 -0700

    bumped up versions of tools

--- setup.py
+++ setup.py
@@ -28,12 +28,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['cellmaps_utils==0.1.0a21',
-                'cellmaps_imagedownloader==0.1.0a13',
-                'cellmaps_ppidownloader==0.1.0a6',
-                'cellmaps_image_embedding==0.1.0a11',
-                'cellmaps_ppi_embedding==0.1.0a7',
-                'cellmaps_coembedding==0.1.0a7',
-                'cellmaps_generate_hierarchy==0.1.0a11',
+                'cellmaps_imagedownloader==0.1.0a14',
+                'cellmaps_ppidownloader==0.1.0a7',
+                'cellmaps_image_embedding==0.1.0a12',
+                'cellmaps_ppi_embedding==0.1.0a8',
+                'cellmaps_coembedding==0.1.0a8',
+                'cellmaps_generate_hierarchy==0.1.0a12',
                 'networkx>=2.8,<2.9',
                 'tqdm']
 
