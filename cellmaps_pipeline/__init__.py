2c5b4fb160acb9df43c472c532e9c73681c2e313
Chris Churas <churas.camera@gmail.com>
2023-12-20 17:10:52 -0800

    bumped to a16

--- cellmaps_pipeline/__init__.py
+++ cellmaps_pipeline/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = 'Ideker Lab Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a15'
+__version__ = '0.1.0a16'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_pipeline'
 __description__ = 'Runs full Cell Maps pipeline'
