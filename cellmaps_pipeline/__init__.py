fc8aad82e20b301942b05537d9d1466621b282ff
Chris Churas <churas.camera@gmail.com>
2024-03-05 17:19:59 -0800

    updated author

--- cellmaps_pipeline/__init__.py
+++ cellmaps_pipeline/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for Cell Maps Pipeline."""
 
-__author__ = 'Ideker Lab Cell Maps team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
 __version__ = '0.1.0a18'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_pipeline'
