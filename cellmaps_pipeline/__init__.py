07af14d6ac41bc5f2a2451101bcf258d0d51dbfb
dzoanka <joanna.lenkiewicz@gmail.com>
2024-12-10 14:31:26 -0800

    Drop alpha

--- cellmaps_pipeline/__init__.py
+++ cellmaps_pipeline/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '1.0.0a1'
+__version__ = '1.0.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_pipeline'
 __description__ = 'Runs full Cell Maps pipeline'
