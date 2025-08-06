a3b58382cc62ddee93ef235198cebd51d7cfda9b
dzoanka <joanna.lenkiewicz@gmail.com>
2025-08-06 11:57:31 -0700

    Added implementation for comunitydetection

--- cellmaps_pipeline/cellmaps_cywebserviceapp.py
+++ cellmaps_pipeline/cellmaps_cywebserviceapp.py
@@ -5,19 +5,25 @@ import json
 import os.path
 import math
 import sys
+import tempfile
+
+import cdapsutil
 import numpy as np
 import pandas as pd
 import logging
 import logging.config
 
+from cellmaps_generate_hierarchy.hierarchy import CDAPSHiDeFHierarchyGenerator
+from cellmaps_generate_hierarchy.maturehierarchy import HiDeFHierarchyRefiner
+from cellmaps_generate_hierarchy.ppi import CosineSimilarityPPIGenerator
+from cellmaps_generate_hierarchy.runner import CellmapsGenerateHierarchy
 from cellmaps_utils import logutils
 from cellmaps_utils import constants
 from cellmaps_utils import music_utils
 
-from ndex2.cx2 import CX2Network
+from ndex2.cx2 import CX2Network, RawCX2NetworkFactory, CX2NetworkPandasDataFrameFactory
 
 import cellmaps_pipeline
-
 
 logger = logging.getLogger(__name__)
 
@@ -35,6 +41,7 @@ def _parse_arguments(desc, args):
     """
     parser = argparse.ArgumentParser(description=desc,
                                      formatter_class=constants.ArgParseFormatter)
+    # TODO: what input supposed to be
     parser.add_argument('input', help='Input data')
     parser.add_argument('--mode', required=True,
                         choices=['networkfromembedding',
@@ -51,6 +58,8 @@ def _parse_arguments(desc, args):
     parser.add_argument('--embedding_cutoff', default=0.1, type=float,
                         help='Cutoff for keeping embedding edges, 0.0 means keep all edges,'
                              '0.1 means keep top 10 percent')
+    parser.add_argument('--interactome_uuid',
+                        help='UUID of input NDEx network hierarchy')
     parser.add_argument('--tempdir', default='/tmp',
                         help='Directory needed to hold files temporarily for processing')
     parser.add_argument('--logconf', default=None,
@@ -71,6 +80,7 @@ def _parse_arguments(desc, args):
                                  cellmaps_pipeline.__version__))
 
     return parser.parse_args(args)
+
 
 def _get_sim_mat_from_similarity(df=None, algorithm='cosine'):
     """
@@ -99,6 +109,7 @@ def _get_sim_mat_from_similarity(df=None, algorithm='cosine'):
         return music_utils.spearman_scaled(df)
 
     raise Exception('Invalid similarity: ' + str(algorithm))
+
 
 def network_from_embedding_mode(embedding=None, algorithm='cosine',
                                 cutoff=0.1):
@@ -136,6 +147,57 @@ def network_from_embedding_mode(embedding=None, algorithm='cosine',
 
     return [new_net.to_cx2()]
 
+
+def community_detection_mode(interactome, ndex_uuid, ppi_cutoffs=CosineSimilarityPPIGenerator.PPI_CUTOFFS,
+                             algorithm=CellmapsGenerateHierarchy.ALGORITHM,maxres=CellmapsGenerateHierarchy.MAXRES,
+                             k=CellmapsGenerateHierarchy.K_DEFAULT,
+                             containment_threshold=HiDeFHierarchyRefiner.CONTAINMENT_THRESHOLD,
+                             jaccard_threshold=HiDeFHierarchyRefiner.JACCARD_THRESHOLD,
+                             min_system_size=HiDeFHierarchyRefiner.MIN_SYSTEM_SIZE,
+                             min_diff=HiDeFHierarchyRefiner.MIN_DIFF):
+
+    tmpdir = tempfile.mkdtemp(prefix='cdetect_')
+
+    factory = RawCX2NetworkFactory()
+    parent_cx2 = factory.get_cx2network(interactome)
+
+    pandas_cx2_factory = CX2NetworkPandasDataFrameFactory()
+    df = pandas_cx2_factory.get_dataframe(parent_cx2)
+    weight_col = next((col for col in df.columns if col.lower() == 'weight'), None)
+    if weight_col is None:
+        raise ValueError("No 'weight' column found (case-insensitive)")
+    df = df.sort_values(weight_col, ascending=False)
+    edgelist_files = []
+    for cutoff in ppi_cutoffs:
+        top_df = df.iloc[:int(len(df) * cutoff)]
+        path = os.path.join(tmpdir, f'ppi_{cutoff}.id.edgelist.tsv')
+        top_df[['source_id', 'target_id']].to_csv(path, sep='\t', index=False, header=False)
+        edgelist_files.append(path)
+
+    outputprefix = os.path.join(tmpdir, CDAPSHiDeFHierarchyGenerator.HIDEF_OUT_PREFIX)
+    hier_generator = CDAPSHiDeFHierarchyGenerator()
+    hier_generator._run_hidef(edgelist_files, outputprefix, algorithm, maxres, k)
+
+    refiner = HiDeFHierarchyRefiner(ci_thre=containment_threshold,
+                                    ji_thre=jaccard_threshold,
+                                    min_term_size=min_system_size,
+                                    min_diff=min_diff)
+
+    refiner.refine_hierarchy(outprefix=outputprefix)
+
+    cdaps_out_file = os.path.join(tmpdir,
+                                  CDAPSHiDeFHierarchyGenerator.CDAPS_JSON_FILE)
+
+    hier_generator = CDAPSHiDeFHierarchyGenerator()
+    with open(cdaps_out_file, 'w') as out_stream:
+        hier_generator.convert_hidef_output_to_cdaps(out_stream, tmpdir)
+
+    cd = cdapsutil.CommunityDetection(runner=cdapsutil.ExternalResultsRunner())
+    hierarchy = cd.run_community_detection(parent_cx2, algorithm=cdaps_out_file, uuid=ndex_uuid)
+
+    return [hierarchy.to_cx2()]
+
+
 def main(args):
     """
     Main entry point for program.
@@ -167,12 +229,27 @@ For more information visit https://cellmaps-pipeline.readthedocs.io
     try:
 
         logutils.setup_cmd_logging(theargs)
+
+        # Mute all logs except CRITICAL - Temporary fix - need to refactor cellmaps_generate_hierarchy
+        # code to be more robust
+        for logname in logging.root.manager.loggerDict:
+            logging.getLogger(logname).setLevel(logging.CRITICAL)
+        logging.getLogger().setLevel(logging.CRITICAL)
+
         if theargs.mode == 'networkfromembedding':
             result = [{'action': 'addNetworks',
                        'data': network_from_embedding_mode(embedding=theargs.embedding,
                                                            algorithm=theargs.similarity,
                                                            cutoff=theargs.embedding_cutoff,
                                                            )}]
+        if theargs.mode == 'communitydetection':
+            result = [{'action': 'addNetworks',
+                       'data': community_detection_mode(interactome=theargs.input,
+                                                        ndex_uuid=theargs.interactome_uuid,
+                                                        ppi_cutoffs=CosineSimilarityPPIGenerator.PPI_CUTOFFS,
+                                                        algorithm=CellmapsGenerateHierarchy.ALGORITHM,
+                                                        maxres=CellmapsGenerateHierarchy.MAXRES,
+                                                        k=CellmapsGenerateHierarchy.K_DEFAULT)}]
         json.dump(result, sys.stdout, indent=2)
     except Exception as e:
         logger.exception('Caught exception: ' + str(e))
